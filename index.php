d56b28ab34eac4bd56b0f1fccfaeca1556cd97cd
Samuel <krewcris@gmail.com>
2023-06-07 09:26:18 +0200

    Creo login y lo modifico junto con index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insertar</title>
+</head>
+    <body>
+        <form action="login.php" method="post">
+            <p><label >Nombre: <input type="text" name="usuario" placeholder="Usuario" required></label></p>
+            
+            <p><label >Contraseña: <input type="password" name="contraseña" placeholder="Contraseña" required></label></p>
+            
+         
+            <input type="submit" value="Aceptar">
+            
+        </form>
+        <p><a href="registrar.php">Registrar nuevo usuario</a></p>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+    //Establezco la conexion a la BD
+    require 'conexion.php';
+    //Me traigo todos los datos de la tabla login
+    $sql= "Select * from login";
+    // Ejecuto la sentencia y guardo el resultado
+    $resultado= $mysqli->query($sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+    <body>
+        <?php
+        //Recojo los datos y creo una nueva variable a false
+            $usuario=$_POST['usuario'];
+            $contraseña=$_POST['contraseña'];
+            $encontrado=false;
+            // en el bucle pongo que mientras que existan datos y encontrado este a falso(sin coincidencias)
+            // siga el bucle tanto como usuarios haya
+
+            //Con este bucle indico que mientras existan datos y encontrado esté a false siga el bucle.
+            while(($fila = $resultado->fetch_assoc()) && ($encontrado==false)){
+                if ($usuario == $fila['usuario'] && $contraseña == $fila['contraseña']){
+                    //Si hay coincidencia imprimo el nombre y pongo encontrado a true para finalizar el bucle
+                        $encontrado=true;
+                        echo "Bienvenido ", $fila['nombre'];
+                        echo ' <br><a href="index.html">Volver al inicio</a>';
+                    }else{
+                        $encontrado=false;
+                    }
+                }
+                // Si no se accede con un usuario de la BD inserto el error.
+                if ($encontrado==false){
+                    echo "Usuario o contraseña incorrecta <br>";
+                    echo '<a href="index.php">Volver al inicio</a>';
+                }
+           
+        
+?>
+
+    </body>
+</html>
